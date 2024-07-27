92d567605c331ee7353d1b021d8301d29ffd3d09
Miguel Angel Villegas <chumilo227@gmail.com>
2024-07-27 15:35:28 -0500

    feat: Update RecipeService to fetch recipes based on search filters

--- src/services/RecipeService.ts
+++ src/services/RecipeService.ts
@@ -1,11 +1,28 @@
 import axios from 'axios';
-import { CategoriesApiResponseSchema } from '../utils/recipes-shema';
+import type { SearchFilter } from '../types';
+import {
+	CategoriesApiResponseSchema,
+	DrinksAPIResponse,
+} from '../utils/recipes-shema';
 
 export async function getCategories() {
 	const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
 	const { data } = await axios(url);
 
 	const result = CategoriesApiResponseSchema.safeParse(data);
+
+	if (result.success) {
+		return result.data;
+	}
+}
+
+export async function getRecipes(filters: SearchFilter) {
+	const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${filters.category}&i=${filters.ingredient}`;
+
+	const { data } = await axios(url);
+
+	const result = DrinksAPIResponse.safeParse(data);
+	console.log(result.success);
 
 	if (result.success) {
 		return result.data;
