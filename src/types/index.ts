cf58b797d022088d818c8f1c3f05b330eb04c73f
Miguel Angel Villegas <chumilo227@gmail.com>
2024-07-27 15:36:01 -0500

    feat: Add DrinksAPIResponse type for handling API response with drinks

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,12 @@
 import { z } from 'zod';
 import {
 	CategoriesApiResponseSchema,
+	DrinksAPIResponse,
 	SearchFilterSchema,
 } from '../utils/recipes-shema';
 
 export type Categories = z.infer<typeof CategoriesApiResponseSchema>;
 
 export type SearchFilter = z.infer<typeof SearchFilterSchema>;
+
+export type Drinks = z.infer<typeof DrinksAPIResponse>;
