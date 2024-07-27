998a367431a7aecff3fcf9c87a7b975ffb93ba40
Miguel Angel Villegas <chumilo227@gmail.com>
2024-07-27 18:41:57 -0500

    feat: Improve IndexPage with lazy loading for images

--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -1,3 +1,30 @@
+import { useMemo } from 'react';
+import DrinkCard from '../Components/DrinkCard';
+import { useAppStore } from '../stores/useAppStore';
+
 export default function IndexPage() {
-	return <div>IndexPage</div>;
+	const { drinks } = useAppStore((state) => state.drinks);
+
+	const hasDrinks = useMemo(() => drinks.length, [drinks]);
+
+	return (
+		<>
+			<h1 className='text-6xl font-extrabold'>Recetas</h1>
+			{hasDrinks ? (
+				<div className='grid grid-cols-1 md:grid-cols-3 2xl:grid-cols-3 my-10 gap-10'>
+					{drinks.map((drink) => (
+						<DrinkCard
+							key={drink.idDrink}
+							drink={drink}
+						/>
+					))}
+				</div>
+			) : (
+				<p className='my-10 text-center text-2xl'>
+					No hay resultados aun, utiliza el formulario para buscar recetas
+				</p>
+			)}
+			<p>This is the index page.</p>
+		</>
+	);
 }
