0ca273916197307a568f5b52bb3243ed58ac8e4a
Miguel Angel Villegas <chumilo227@gmail.com>
2024-07-27 18:42:27 -0500

    create new type api response

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 import {
 	CategoriesApiResponseSchema,
+	DrinkAPIResponse,
 	DrinksAPIResponse,
 	SearchFilterSchema,
 } from '../utils/recipes-shema';
@@ -10,3 +11,5 @@ export type Categories = z.infer<typeof CategoriesApiResponseSchema>;
 export type SearchFilter = z.infer<typeof SearchFilterSchema>;
 
 export type Drinks = z.infer<typeof DrinksAPIResponse>;
+
+export type Drink = z.infer<typeof DrinkAPIResponse>;
