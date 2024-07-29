8117c446e21cf7d2ce03b331badb49abc84b8671
Miguel Angel Villegas <chumilo227@gmail.com>
2024-07-29 17:49:18 -0500

    refactor: Update useAppStore to include favoriteSlice

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,9 +1,11 @@
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
 import { createRecipeSlice, RecipeSliceType } from './recipeSlice';
+import { createFavoriteSlice, type FavoriteSliceType } from './favoriteSlice';
 
-export const useAppStore = create<RecipeSliceType>()(
+export const useAppStore = create<RecipeSliceType & FavoriteSliceType>()(
 	devtools((...a) => ({
 		...createRecipeSlice(...a),
+		...createFavoriteSlice(...a),
 	}))
 );
