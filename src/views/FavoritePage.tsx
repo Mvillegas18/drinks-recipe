34e1fd88f717511747219577df3e2016c8b51370
Miguel Angel Villegas <chumilo227@gmail.com>
2024-07-30 15:49:04 -0500

    refactor: Update FavoritePage to display favorite drinks and handle empty favorites

--- src/views/FavoritePage.tsx
+++ src/views/FavoritePage.tsx
@@ -1,3 +1,28 @@
+import { useMemo } from 'react';
+import DrinkCard from '../Components/DrinkCard';
+import { useAppStore } from '../stores/useAppStore';
+
 export default function FavoritePage() {
-	return <div>FavoritePage</div>;
+	const { favorites } = useAppStore();
+
+	const hasFavorite = useMemo(() => favorites.length, [favorites]);
+	return (
+		<>
+			<h1 className='text-6xl font-extrabold'>FavoritePage</h1>
+			{hasFavorite ? (
+				<div className='grid grid-cols-1 md:grid-cols-3 2xl:grid-cols-3 my-10 gap-10'>
+					{favorites.map((drink) => (
+						<DrinkCard
+							key={drink.idDrink}
+							drink={drink}
+						/>
+					))}
+				</div>
+			) : (
+				<p className='my-10 text-center text-2xl'>
+					No hay favoritos aun, agrega bebidas a favoritos
+				</p>
+			)}
+		</>
+	);
 }
