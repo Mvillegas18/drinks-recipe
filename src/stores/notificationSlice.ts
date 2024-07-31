ee8fc200272d1f77e99fff74face75a1b4730460
Miguel Angel Villegas <chumilo227@gmail.com>
2024-07-31 11:37:18 -0500

    refactor: Update Notification component to include hideNotification function

--- src/stores/notificationSlice.ts
+++ src/stores/notificationSlice.ts
@@ -1,4 +1,5 @@
 import { StateCreator } from 'zustand';
+import { FavoriteSliceType } from './favoriteSlice';
 
 export type Notification = {
 	text: string;
@@ -8,14 +9,38 @@ export type Notification = {
 
 export type NotificationSliceType = {
 	notification: Notification;
+	showNotification: (payload: Pick<Notification, 'text' | 'error'>) => void;
+	hideNotification: () => void;
 };
 
-export const createNotificationSlice: StateCreator<NotificationSliceType> = (
-	set
-) => ({
+export const createNotificationSlice: StateCreator<
+	NotificationSliceType & FavoriteSliceType,
+	[],
+	[],
+	NotificationSliceType
+> = (set, get) => ({
 	notification: {
 		text: '',
 		error: false,
 		show: false,
+	},
+	showNotification: (payload) => {
+		set({
+			notification: {
+				text: payload.text,
+				error: payload.error,
+				show: true,
+			},
+		});
+		setTimeout(() => get().hideNotification(), 5000);
+	},
+	hideNotification: () => {
+		set({
+			notification: {
+				text: '',
+				error: false,
+				show: false,
+			},
+		});
 	},
 });
